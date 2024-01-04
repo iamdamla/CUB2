9653714b544e8d087d42f6899e017e6e488c6f7b
Damla <38767978+iamdamla@users.noreply.github.com>
2024-01-04 14:54:31 +0100

    Add files via upload

--- screen.c
+++ screen.c
@@ -5,62 +5,85 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: derblang <derblang@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/02 12:39:03 by derblang          #+#    #+#             */
-/*   Updated: 2024/01/03 15:51:15 by derblang         ###   ########.fr       */
+/*   Created: 2024/01/04 12:36:38 by derblang          #+#    #+#             */
+/*   Updated: 2024/01/04 14:44:03 by derblang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+
 #include "../cub3d.h"
 
-// void build_wall(t_mlx *mlx)
+// void draw_screen(t_mlx *mlx)
 // {
-//     size_t height;
-//    t_ray *ray = &(mlx->raycast);
+//     int x;
+//     int y;
+//     t_ray *ray = &(mlx->raycast);
 
-//     int x = 0;
-//     int y = 0;
-//     while(x < WIDTH - 1)
+//     x = 0;
+//     y = (HEIGHT * 64) / ray->dist_player_screen;
+//     printf("y-> %d\n", y);
+//     while(x < WIDTH)
 //     {
-//         y = 0;
-//         while(y < HEIGHT - 1)
+//         while(y < HEIGHT)
 //         {
-//            height = (4000 / ray->dist_player_screen);
-//            y = 0 - height;
-//            y++;
+//             mlx_put_pixel(mlx->screen,x,y,G025);
+//             y++;
+            
 //         }
 //         x++;
 //         y = 0;
-//     } 
-//     printf("height -> %ld\n", height);
-//     printf("ray->dist_player %d\n",ray->dist_player_screen);
-//     printf("ray->dist_ray %d\n", ray->dist_ray);
-   
+//     }
 // }
 
 
-void build_wall(t_mlx *mlx)
-{
-    size_t height;
-    t_ray *ray = &(mlx->raycast);
+// static double	deg2rad(int deg)
+// {
+// 	return (deg * (M_PI / 180));
+// }
 
-    int x = 0;
-    int y = 0;
-    
-    while(x < WIDTH - 1)
+// void draw_screen(t_mlx *mlx)
+// {
+//     t_ray *ray = &(mlx->raycast);
+//     int x = 0;
+//     int y =0;
+//     int line_height = (int)(HEIGHT / ray->dist_ray);
+//     int draw_start = -line_height / 2 + HEIGHT / 2;
+//     if(draw_start < 0)
+//         draw_start = 0;
+//     int draw_end = line_height / 2 + HEIGHT / 2;
+//     if(draw_end >= HEIGHT)
+//         draw_end = HEIGHT - 1;
+//     verLine(x, draw_start, draw_end, G025);
+//     draw_line(mlx, draw_start, draw_end, double angle)
+//     while(x < WIDTH)
+//     {
+//         while(y < HEIGHT)
+//         {
+//             mlx_put_pixel(mlx->img,x,y,G025);
+//             y++;
+//         }
+//         x++;
+//         y = 0;
+//     }
+// }
+
+void draw_screen(t_mlx *mlx)
+{
+    int x;
+    int y;
+    t_ray *ray = &(mlx->raycast);
+    ray->fish_eyes = 
+    x = 0;
+    y = (HEIGHT * 64) / ray->dist_player_screen;
+    while(x < WIDTH)
     {
-         
-        while(y < HEIGHT - 1)
+        while(y < HEIGHT)
         {
-            height = ((64.0 / ray->dist_player_screen) * 277.0);
-            y = 0 - height;   
-            mlx_put_pixel(mlx->screen, x, y, G025);
+            mlx_put_pixel(mlx->screen,x,y,G025);
             y++;
+            
         }
         x++;
         y = 0;
     }
-     printf("height -> %ld\n", (size_t)height);
-   // printf("y-> %d\n", y);
-    // printf("ray->dist_player %d\n",ray->dist_player_screen);
-    // printf("ray->dist_ray %d\n", ray->dist_ray);
 }
